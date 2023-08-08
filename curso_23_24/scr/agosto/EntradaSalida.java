ac4cb438aec0ab541d256f864b1d37ad388af199
israelPlaza <israel.plaza.mayoral@gmail.com>
2023-08-08 13:44:55 +0200

    dia1 mañana

---
+++ curso_23_24/scr/agosto/EntradaSalida.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class EntradaSalida{
+public static void main(String[] args) {
+    
+    Scanner entrada=new Scanner(System.in);
+    
+    System.out.println("Escribe tu nombre");
+    String nombre=entrada.nextLine();
+    System.out.println("la edad");
+    int edad=entrada.nextInt();
+    System.out.println(nombre +" "+ edad);
+
+
+
+}
+
+}
